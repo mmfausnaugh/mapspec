e91e1e7d34affc4f7827499fa770d4f3c1e8fd65
Michael Fausnaugh <faus@CSR-DYN-01.MIT.EDU>
2017-08-09 15:45:36 -0400

    updated readme

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(
     author='Michael Fausnaugh',
     author_email='faus@mit.edu',
     url='https://github.com/mmfausnaugh/mapspec',
-    license='GPLv3',
+    license='MIT',
     packages=['mapspec'],
     scripts=['scripts/make_ref', 'scripts/smooth_ref', 'scripts/do_map'],
 #    package_data = ['examples/test_data/', 'examples/mapspec_test' ]
